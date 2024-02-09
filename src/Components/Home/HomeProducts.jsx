65d0070bb68f5073cb6a6046b18aa0945a5d34da
justinait <justiiturraspe@gmail.com>
2024-02-09 19:33:47 -0300

    home buttons

--- src/Components/Home/HomeProducts.jsx
+++ src/Components/Home/HomeProducts.jsx
@@ -7,59 +7,60 @@ import shorts from '/images/shorts.png'
 import camisas from '/images/pantalones.jpg'
 import hoodies from '/images/hoodie.png'
 import accesorios from '/images/accesorios.png'
+import { Link } from 'react-router-dom'
 
 function HomeProducts() {
   return (
     <div>
-        <div className='productHomeBox'>
+        <Link to='/tshirts' className='productHomeBox'>
             <img src={tshirts} alt="Remeras" className='imageProductHome' />
             <div className='homeProductInfo'>
                 <p className='productTitleHome'>Remeras</p>
                 <p className='seeInfoHome'>Ver</p>
             </div>
-        </div>
-        <div className='productHomeBox'>
+        </Link>
+        <Link to='/category' className='productHomeBox'>
             <img src={trausers} alt="Pantalones" className='imageProductHome' />
             <div className='homeProductInfo'>
                 <p className='productTitleHome'>Pantalones</p>
                 <p className='seeInfoHome'>Ver</p>
             </div>
-        </div>
-        <div className='productHomeBox'>
+        </Link>
+        <Link to='/category' className='productHomeBox'>
             <img src={camisas} alt="Camisas" className='imageProductHome' />
             <div className='homeProductInfo'>
                 <p className='productTitleHome'>Camisas</p>
                 <p className='seeInfoHome'>Ver</p>
             </div>
-        </div>
-        <div className='productHomeBox'>
+        </Link>
+        <Link to='/shorts' className='productHomeBox'>
             <img src={shorts} alt="Bermudas" className='imageProductHome' />
             <div className='homeProductInfo'>
                 <p className='productTitleHome'>Bermudas</p>
                 <p className='seeInfoHome'>Ver</p>
             </div>
-        </div>
-        <div className='productHomeBox'>
+        </Link>
+        <Link to='/category' className='productHomeBox'>
             <img src={buzos} alt="Buzos" className='imageProductHome' />
             <div className='homeProductInfo'>
                 <p className='productTitleHome'>Buzos</p>
                 <p className='seeInfoHome'>Ver</p>
             </div>
-        </div>
-        <div className='productHomeBox'>
+        </Link>
+        <Link to='/category' className='productHomeBox'>
             <img src={hoodies} alt="Hoodies" className='imageProductHome' />
             <div className='homeProductInfo'>
                 <p className='productTitleHome'>Hoodies</p>
                 <p className='seeInfoHome'>Ver</p>
             </div>
-        </div>
-        <div className='productHomeBox'>
+        </Link>
+        <Link to='/category' className='productHomeBox'>
             <img src={accesorios} alt="Accesorios" className='imageProductHome' />
             <div className='homeProductInfo'>
                 <p className='productTitleHome'>Accesorios</p>
                 <p className='seeInfoHome'>Ver</p>
             </div>
-        </div>
+        </Link>
     </div>
   )
 }
