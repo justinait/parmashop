02f7ee5b5dc92b373e08ca2202bed9afcf411ad4
justinait <justiiturraspe@gmail.com>
2024-02-13 17:51:38 -0300

    sin stok css

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -33,6 +33,8 @@ function Products() {
           return(
             <Link to={`/item/${e.id}`} className='productContainer' key={i}>
               <img src={e.image} alt={e.name} className='imgProduct'/>
+              {!e.stock && <p className='stockProducts'>Sin Stock</p>}
+              {/* en realidad tengo q hacer un map que revise el stock de todos los colores  */}
               <div className='productInfoContainer'>
                 <p className='productName'>{e.name}</p>
                 <p className='productPrice'>$ {e.unit_price}</p>
