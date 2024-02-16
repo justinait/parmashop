6a11d3d52908c5e14e7d80164c5742107bc4a16c
justinait <justiiturraspe@gmail.com>
2024-02-16 12:13:49 -0300

    correcting

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -13,7 +13,7 @@ import {v4} from "uuid"
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTH,
   projectId: import.meta.env.VITE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGING_ID,
