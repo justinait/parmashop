8abb0d31e0f2eeb01a2fc5d1de10579c94394d1d
justinait <justiiturraspe@gmail.com>
2024-03-22 13:37:56 -0300

    dashboard order

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -8,18 +8,20 @@ function Dashboard() {
 
   const [isChange, setIsChange] = useState(false)
   const [products, setProducts] = useState([]);
-  
+        
   useEffect(()=> {
     setIsChange(false)
 
-    let productsCollection = collection(db, "products");
+    const productsCollection = collection(db, "products");
     getDocs(productsCollection).then(res =>{
-      let newArr = res.docs.map(product=>{
+      const newArr = res.docs.map(product=>{
         return {
           ...product.data(),
           id: product.id
         }
       })
+      const productsWithNames = newArr.filter(product => product.name);
+      productsWithNames.sort((a, b) => a.name.localeCompare(b.name));
       setProducts(newArr)
     })
   }, [isChange])
@@ -28,7 +30,6 @@ function Dashboard() {
     <div className='dashboardContainer' style={{minHeight:'90vh'}}>
       
       <h2>Dashboard de Administrador</h2>
-      
       <ProductsDashboard products={products} setIsChange={setIsChange} /> 
       
     </div>
